bfc38f728c1d9736b1ee8db610a5d58f762b78c0
RuoguGao <Ruogu.Gao@outlook.com>
2017-01-18 14:35:56 +0800

    pyBoost_002, local small set test passed, submitting to large scale datasets on Apollo.

--- ConvMonitor_Relion_Mk05_pyBoost.py
+++ ConvMonitor_Relion_Mk05_pyBoost.py
@@ -25,7 +25,7 @@
 #   9.CYC, column number of  _rlnCoordinateY                     
 #   10.MNC, column number of  _rlnMicrographName                 
 #   11.CNC, column number of  _rlnClassNumber                
-#   12.NCC, ~ _rlnNormCorrection                
+#   12._NCC, ~ _rlnNormCorrection                
 #   13.LLCC, ~ _rlnLogLikeliContribution        
 #   14.MVPDC, ~ _rlnMaxValueProbDistribution    
 #   15.NSSC, ~ _rlnNrOfSignificantSamples       
@@ -40,6 +40,7 @@ import sys
 import os
 import math
 import logging
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
@@ -54,7 +55,7 @@ STAT_FILE       = 'Migration' + '_' + VERSION + '.dat'
 
 PROC_UPPER = 32
 PROC_LOWER = 1
-
+'''
 EXTRACTN=100
 HEADERL=28
 TCOLUMN=20
@@ -68,7 +69,7 @@ LLCC=22     #_rlnLogLikeliContribution #22
 MVPDC=23    #_rlnMaxValueProbDistribution #23 
 NSSC=24     #_rlnNrOfSignificantSamples #24 
 TEST_FLAG=0
-
+'''
 logging.basicConfig( filename = VERSION + '.log', level = logging.DEBUG )
 
 def _DataLoader():
@@ -79,23 +80,20 @@ def _DataLoader():
         return ( 'Invalid file path' )
 
     conf = f.readlines()
-    
-    process_N   = conf[1].strip().split(' ')[1]
-    pool_size   = conf[2].strip().split(' ')[1]
-    EXTRACTN    = conf[3].strip().split(' ')[1] #100
-    HEADERL     = conf[4].strip().split(' ')[1] #28
-    TCOLUMN     = conf[5].strip().split(' ')[1] #20
-    INC         = conf[6].strip().split(' ')[1] #10 #_rlnImageName #10
-    CXC         = conf[7].strip().split(' ')[1] #_rlnCoordinateX #11 
-    CYC         = conf[8].strip().split(' ')[1] #12 #_rlnCoordinateY #12 
-    MNC         = conf[9].strip().split(' ')[1] #13 #_rlnMicrographName #13 
-    CNC         = conf[10].strip().split(' ')[1] #20 #_rlnClassNumber #20  
-    NCC         = conf[11].strip().split(' ')[1] #21 #_rlnNormCorrection #21
-    LLCC        = conf[12].strip().split(' ')[1] #22 #_rlnLogLikeliContribution #22 
-    MVPDC       = conf[13].strip().split(' ')[1] #23 #_rlnMaxValueProbDistribution #23 
-    NSSC        = conf[14].strip().split(' ')[1] #24 #_rlnNrOfSignificantSamples #24 
-    TEST_FLAG   = conf[15].strip().split(' ')[1]
+    global process_N, pool_size, EXTRACTN, HEADERL, CNC, NCC, LLCC, MVPDC, NSSC, TEST_FLAG
 
+    process_N   = int(conf[1].strip().split(' ')[1] )
+    pool_size   = int(conf[2].strip().split(' ')[1] )
+    EXTRACTN    = int(conf[3].strip().split(' ')[1] ) #100
+    HEADERL     = int(conf[4].strip().split(' ')[1] ) #28 
+    CNC         = int(conf[5].strip().split(' ')[1] ) #20 #_rlnClassNumber #20  
+    NCC         = int(conf[6].strip().split(' ')[1] ) #21 #_rlnNormCorrection #21
+    LLCC        = int(conf[7].strip().split(' ')[1] ) #22 #_rlnLogLikeliContribution #22 
+    MVPDC       = int(conf[8].strip().split(' ')[1] ) #23 #_rlnMaxValueProbDistribution #23 
+    NSSC        = int(conf[9].strip().split(' ')[1] ) #24 #_rlnNrOfSignificantSamples #24 
+    TEST_FLAG   = int(conf[10].strip().split(' ')[1] )
+
+    logging.info( 'Waypoint/> ' + 'conf reading done, T_column ' + str( CNC ) )
     ##para-checker
     if ( process_N > PROC_UPPER or process_N < PROC_LOWER ):
         logging.info('Invalid process number.')
@@ -111,21 +109,21 @@ def _DataLoader():
         file_list.append( str( sys.argv[i] ) )
 
     ##oganize output bundle
-    result_bundle = ( file_list, process_N, pool_size )
+    result_bundle = ( file_list, process_N, pool_size, (HEADERL, CNC, NCC, LLCC, MVPDC, NSSC) )
 
-    return result
+    return result_bundle
 
 
-def _TaskSplitter( file_list, _pN ):
+def _TaskSplitter( file_list, piece_N, paraMs ):
     file_N = len(file_list)
     if ( len(file_list) < piece_N ):
         logging.info( 'Gentlemen, please make sure that the number of files are LARGER than the processes expected to ultilise, otherwise all those effort would be for nothing.' )
         exit(3)
-    piece_length = file_N//_pN
+    piece_length = file_N//piece_N
 
     re_list = []
     for index in range(piece_N-1):
-        re_list.append( index, (,), [] )
+        re_list.append( (index, [1,2], [], paraMs) )
         re_list[index][1][0] = index*piece_length
         re_list[index][1][1] = (index+1)*piece_length
         #re_list.append( ( index, index*piece_length, (index+1)*piece_length ) )
@@ -133,9 +131,9 @@ def _TaskSplitter( file_list, _pN ):
             re_list[index][2].append( file_list[j] )
 
     #re_list.append( ( (piece_N-1), (piece_N-1)*piece_length, file_N ) ) #the last entry
-    re_list[piece_N-1] = ( piece_N-1, (,),[] )
-    re_list[piece_N-1][1][0] = index*piece_length
-    re_list[piece_N-1][1][1] = (index+1)*piece_length
+    re_list.append( (piece_N-1, [1,2], [], paraMs) )
+    re_list[piece_N-1][1][0] = (piece_N-1)*piece_length
+    re_list[piece_N-1][1][1] = file_N
     for j in range( (piece_N-1)*piece_length, file_N ):
         re_list[piece_N-1][2].append( file_list[j] )
 
@@ -148,20 +146,47 @@ def _StandAloneModule( in_bundle ):
     sequence_N = in_bundle[0]
     files = in_bundle[2]
     interval = in_bundle[1]
+    paraMs = in_bundle[3]
     logging.info("Waypoint/> process_{0} running...".format(os.getpid()))
-    #organize
-    result = ( [[]], [[]], [[]], [[]], [[]] ) #class, NC, NSS, MVPD, LLC  
+    #HEADERL, CNC, NCC, LLCC, MVPDC, NSSC
+    _HEADERL = paraMs[0]
+    _NCC = paraMs[2]
+    _CNC = paraMs[1]
+    _NSSC = paraMs[5]
+    _MVPDC = paraMs[4]
+    _LLCC = paraMs[3]
+    #TAKE NOTE:  [[0 for col in range(n)] for row in range(n)] to organise lists as matrix
+    #and the following result is actually a tensor...
+    file_buffer = open( files[0], 'r' )
+    data_buffer = file_buffer.readlines()
+    row_L = len(data_buffer) - _HEADERL
+    col_L = len(files)
+    logging.info("Waypoint/process_{0}/>file_list:{1}, row:{2}, col:{3}, header_length:{4}, CNC:{5}".format(os.getpid(), str(files), row_L, col_L, int(_HEADERL), int(_CNC)))
+
+    result = (  [[0 for col in range(col_L)] for row in range(row_L)], \
+                [[0 for col in range(col_L)] for row in range(row_L)], \
+                [[0 for col in range(col_L)] for row in range(row_L)], \
+                [[0 for col in range(col_L)] for row in range(row_L)], \
+                [[0 for col in range(col_L)] for row in range(row_L)], \
+             ) #class, NC, NSS, MVPD, LLC  //////////////////////// POTENTIAL-HAZARD
     for i in range( len(files) ):
         #open file
         file_buffer = open( files[i], 'r' )
         data_buffer = file_buffer.readlines()
         #extract target fields
-        for j in range( HEADERL ,len(data_buffer) ): #NOTE: here we assume that particle number differs not between files
-            result[0][j][i] = data_buffer[j].strip().split(' ')[TCOLUMN-1]
-            result[1][j][i] = data_buffer[j].strip().split(' ')[NCC-1]
-            result[2][j][i] = data_buffer[j].strip().split(' ')[NSSC-1]
-            result[3][j][i] = data_buffer[j].strip().split(' ')[MVPDC-1]
-            result[4][j][i] = data_buffer[j].strip().split(' ')[LLCC-1]
+        #logging.info( "Waypoint/process_{0}/>file:{1}, row:{2}, col:{3}, cnc:{4}".format(os.getpid(), str(files[i]), len(data_buffer) - _HEADERL, len(files), _CNC ))
+        for j in range( _HEADERL ,len(data_buffer) ): #NOTE: here we assume that particle number differs not between files
+            #logging.info('closing-in/{0}/>( {1}, {2}) while ( {3}, {4})'.format( os.getpid(), j-_HEADERL, i, row_L, col_L ))
+            #logging.info( ' additional/{0}/>{1}-{2}--{3}, while-{4}'.format(os.getpid(), j-_HEADERL, i, _CNC-1, len( data_buffer[j].strip().split(' ') )))
+            result[0][j-_HEADERL][i] = data_buffer[j].strip().split()[_CNC-1]
+            logging.info('closing-in/{0}/>step_1, i = {1}, j = {2}, readin: {3}'.format(os.getpid(), i,j, result[0][j-_HEADERL][i] ))
+            result[1][j-_HEADERL][i] = data_buffer[j].strip().split()[_NCC-1]
+            #logging.info('closing-in/{0}/>step_2, i = {1}, j = {2}'.format(os.getpid(), i,j))
+            result[2][j-_HEADERL][i] = data_buffer[j].strip().split()[_NSSC-1]
+            #logging.info('closing-in/{0}/>step_3, i = {1}, j = {2}'.format(os.getpid(), i,j))
+            result[3][j-_HEADERL][i] = data_buffer[j].strip().split()[_MVPDC-1]
+            #logging.info('closing-in/{0}/>step_4, i = {1}, j = {2}'.format(os.getpid(), i,j))
+            result[4][j-_HEADERL][i] = data_buffer[j].strip().split()[_LLCC-1]
 
         file_buffer.close()
 
@@ -175,13 +200,15 @@ def _Merge( raw_1, raw_2 ):
     for n in range(5): #5 statistic attributes
         for i in range( len(raw_2[n]) ): #raw_1 and raw_2 are supposed to be the same
             for j in range( len(raw_2[n][0]) ):
-                raw_1[n][i][j+len(raw_2[n])] = raw_2[n][i][j]
+                #raw_1[n][i][j+len(raw_1[n][0])] = raw_2[n][i][j]
+                raw_1[n][i].append( raw_2[n][i][j] )
 
     return raw_1
 
 
 def _PostProcess( to_write ):
     #
+    logging.info('Waypoint/postprocess/> ' + str( to_write ))
     w_file = open( STAT_FILE, 'w')
     for i in range( len(to_write) ):
         temp=''
@@ -196,8 +223,8 @@ def _PostProcess( to_write ):
 def _AdditionalAnalysis( trend ):
     #
     tlength = len(trend)
-    for i in range( tlength ):
-        plt.plot( tlength, trend[i] )
+    #ph: might need to convrt str->int manually
+    plt.plot( range(tlength), trend )
 
     plt.savefig( VERSION + '_migration_trend' +'.png')
     return 0
@@ -211,14 +238,17 @@ def main():
     file_list = f_input[0]
     process_N = f_input[1]
     pool_size = f_input[2]
+    macro_parameters = f_input[3]
 
     ####task split
     ####@_TaskSplitter
-    task_bundle = _TaskSplitter( file_list, process_N )
+    task_bundle = _TaskSplitter( file_list, process_N, macro_parameters )
 
     ####Pool-party !!!
+    logging.info( 'Waypoint/> _TaskSplitter done: ' + str(task_bundle) )
+
     work_pool = Pool(pool_size)
-    tobe_merged = work_pool.map(_StandAloneModule, task_bundle)
+    tobe_merged = work_pool.map(_StandAloneModule, task_bundle )
     
     logging.info( "Waypoint/> " + 'joined' )
 
@@ -233,7 +263,7 @@ def main():
 
     end_t = time.time()
 
-    logging.info( "Waypoint/> " + 'file operation done, costs: ' + str( end_t - start_t )  )
+    logging.info( "Waypoint/> " + 'file operation done, time consumption: ' + str( end_t - start_t )  )
     ####Additional analysis, including plotting with GNUPLOT and extract rogue/prudent particles(need additional input parameters)
     ####@_AdditionalAnalysis()
     _AdditionalAnalysis( merged_task[0] )
